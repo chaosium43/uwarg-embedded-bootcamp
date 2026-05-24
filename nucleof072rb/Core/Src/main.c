ea925b9417cf42073dcddcbd1ec39be2cea9d292
chaosium43 <pyrokek420@gmail.com>
2026-05-23 21:22:12 -0400

    Bootcamp: Carlton Qian

--- nucleof072rb/Core/Src/main.c
+++ nucleof072rb/Core/Src/main.c
@@ -19,12 +19,13 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "spi.h"
+#include "tim.h"
 #include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -44,7 +45,9 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-
+const float COUNTER_PERIOD = 40000.0;
+const float DUTY_5P = COUNTER_PERIOD  / 20.0; // diving by whole numbers only for now so as to not cause floating point errors
+const float DUTY_10P = COUNTER_PERIOD / 10.0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -64,6 +67,7 @@ void SystemClock_Config(void);
   */
 int main(void)
 {
+
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
@@ -87,8 +91,11 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_SPI1_Init();
+  MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 40000 / 100 * 5); // set duty cycle to 5% by default
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -96,8 +103,23 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
     /* USER CODE BEGIN 3 */
+	  HAL_Delay(10);
+	  uint8_t inputFrame[] = {0b11000000, 0, 0}; // since frame size is 8 bits, we want the last input frame to be 0 or a new transmission might accidentally be started :'(
+	  uint8_t outputFrame[] = {0, 0, 0};
+	  HAL_SPI_TransmitReceive(&hspi1, inputFrame, outputFrame, 3, HAL_MAX_DELAY); // 17 bits / 8 bits a frame yields 3 frames to transmit and receive at once
+	  uint16_t output = outputFrame[0] & 1; // value the ADC sent back, B9 lives on the last bit of frame 0 btw
+	  output <<= 8;
+	  output += outputFrame[1]; // bits 8 to 1 live in frame 1
+	  output <<= 8;
+	  output += (outputFrame[2] & 0x80) >> 7; // taking B0 from the last output frame and the rest is just garbage input
+
+	  // output should be something from 0 to 1023, map 0 to 5% duty cycle, 1023 to 10% duty cycle
+	  float weight10p = output / 1023.0;
+	  float weight5p = 1.0 - weight5p;
+	  float floatPwmCounter = DUTY_5P * weight5p + DUTY_10P * weight10p;
+	  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, (uint32_t)floatPwmCounter);
+
   }
   /* USER CODE END 3 */
 }
@@ -122,6 +144,7 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+
   /** Initializes the CPU, AHB and APB buses clocks
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
@@ -160,8 +183,7 @@ void Error_Handler(void)
   }
   /* USER CODE END Error_Handler_Debug */
 }
-
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
   *         where the assert_param error has occurred.
@@ -177,5 +199,3 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
